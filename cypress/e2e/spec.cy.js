0db43da379dca84ceaddaa2a24bd5e8ca9cf1e11
natured_amends0protonmail.com <natured_amends0p@icloud.com>
2022-12-13 07:55:45 +0000

    working

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,27 @@
+const crypto = require('crypto')
+
+const routes = [
+    "/post/emoji-support/",
+    "/post/marksdown-syntax/",
+    "/post/math-typesetting/",
+    "/post/placeholder-text/",
+    "/post/rich-content/",
+    "/"
+]
+
+const routeToHash = (route)  =>
+    crypto.createHash('md5').update(route).digest('hex');
+
 describe('Visuals', () => {
-    it('home', () => {
-        cy.visit('/post/math-typesetting/')
+    routes.map(r => ({
+        name: r,
+        hash: routeToHash(r),
+    })).forEach(r => it(r.hash + r.name, () => {
+        cy.visit(r.name)
         const retryOptions = {
             limit: 5,
             delay: 500
         }
-        cy.compareSnapshot('home-page', 0, retryOptions)
-    })
+        cy.compareSnapshot(r.hash + r.name.replace('/', '-'), 0, retryOptions)
+    }))
 })
