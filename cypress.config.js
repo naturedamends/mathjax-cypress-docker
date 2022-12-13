c7ee598ecb010f6bfb3d314395defbe7568cc529
natured_amends0protonmail.com <natured_amends0p@icloud.com>
2022-12-13 03:39:35 +0000

    use workflows.

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 const getCompareSnapshotsPlugin = require('cypress-image-diff-js/dist/plugin');
 module.exports = defineConfig({
   e2e: {
-	  baseUrl: 'http://site.test.dev',
+	  baseUrl: 'http://localhost:80',
     setupNodeEvents(on, config) {
       getCompareSnapshotsPlugin(on, config);
     },
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -xe 
-docker-compose down
-docker-compose build 
-docker-compose run runner sh -c 'npm run ci'
-docker-compose up -d server
-docker-compose run runner sh -c "npm run test"
